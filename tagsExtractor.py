e477b91d9b62a0c3071aed2eb969f07cae82bea2
sarimbinwaseem <sarimbinwaseem2000@gmail.com>
2022-03-22 10:41:27 +0500

    Initial Push

---
+++ tagsExtractor.py
@@ -0,0 +1,34 @@
+import yt_dlp
+import datetime
+from os import chdir, system, listdir
+import json
+
+folderName = str(datetime.datetime.today()).replace(" ", "")
+system(f"mkdir {folderName}")
+chdir(folderName)
+
+ydl_opts = {"skip_download": True,
+			"writeinfojson": True,
+            "no_warnings": True,
+            # "overwrites": False,
+            "nooverwrites": True,
+            "ignoreerrors" : True
+ }
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=qvFG8J7SUDE'])
+
+for file in listdir():
+    if file.endswith(".json"):
+        try:
+            with open(file) as f:
+                data = json.load(f)
+                
+            oneVideoData = data["tags"]
+
+            print(type(oneVideoData))
+            print(",".join(oneVideoData))
+        
+
+        except Exception as e:
+            print(str(e))
